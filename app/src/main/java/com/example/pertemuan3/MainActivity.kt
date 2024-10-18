be1db87af366d3512ee93e476fda021cd4f9f25f
RanggaHijriawanHaryadi <127277495+RanggaHijriawanHaryadi@users.noreply.github.com>
2024-10-18 23:05:30 +0700

    Memambahkan, mengatur style image dengan nama a

--- app/src/main/java/com/example/pertemuan3/MainActivity.kt
+++ app/src/main/java/com/example/pertemuan3/MainActivity.kt
@@ -10,12 +10,15 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontWeight
@@ -75,6 +78,9 @@ fun BasicComposble(modifier: Modifier = Modifier){
             fontSize = 40.sp,
             fontWeight = FontWeight.Bold,
         )
+        Image(painter = painterResource(id = R.drawable.a),
+            contentDescription = null,
+            modifier = Modifier.size(400.dp).padding(2.dp).clip(CircleShape))
     }
     //Text(text = "Hallo android")
 }
